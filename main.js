a7b11807b552980212e9b176cd59774774120399
AshishCoderWhiteHat <71976163+AshishCoderWhiteHat@users.noreply.github.com>
2021-06-28 20:40:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,8 +23,27 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  video = createCapture(VIDEO);
+  video.size(600,300);
+
+  poseNet = ml5.poseNet(video,modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
+function modelLoaded() {
+
+  console.log('modelLoaded!');
+}
+
+function gotPoses() {
+
+  if(results,length > 0)
+    {
+      noseX = results[0].pose.x;
+      noseY = results[0].pose.y;
+      console.log("noseX = " + noseX +", noseY = " + noseY);
+    }
+}
 
 function draw(){
 
